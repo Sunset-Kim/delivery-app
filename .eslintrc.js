794ef007458fd3f19582dee38ca77d8b192ebb2b
Sunset-Kim <0chickim@gmail.com>
2023-04-02 01:33:41 +0900

    config: eslint rule, prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   rules: {
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
+    'object-curly-spacing': ['error', 'always'],
   },
 };
