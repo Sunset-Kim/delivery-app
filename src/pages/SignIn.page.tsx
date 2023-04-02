6bbd0f8d6b030313f5bbf6b090fd8f803282b1e3
Sunset-Kim <0chickim@gmail.com>
2023-04-02 21:15:04 +0900

    feat: keyboard avoid view

--- src/pages/SignIn.page.tsx
+++ src/pages/SignIn.page.tsx
@@ -2,6 +2,7 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { useRef, useState } from 'react';
 import {
   Alert,
+  Dimensions,
   Pressable,
   StyleSheet,
   Text,
@@ -9,6 +10,8 @@ import {
   View,
 } from 'react-native';
 import { RootStackParamList } from '../../App';
+
+import { DismissKeyBoardView } from '../feature/ui/dismiss-keyboard';
 import { useInput } from '../feature/ui/hooks';
 import { TextField } from '../feature/ui/text-field';
 
@@ -34,62 +37,65 @@ const SignInPage = ({
   };
 
   return (
-    <View style={styles.form}>
-      <View>
-        <TextField
-          ref={emailRef}
-          placeholder="아이디를 입력하세요"
-          label="아이디"
-          value={id}
-          onChangeText={setId}
-          importantForAutofill="yes"
-          textContentType="emailAddress"
-          returnKeyType="next"
-          onSubmitEditing={() => passwordRef.current?.focus()}
-          blurOnSubmit={false}
-          keyboardType="email-address"
-        />
-        <TextField
-          label="비밀번호"
-          ref={passwordRef}
-          secureTextEntry
-          placeholder="비밀번호를 입력하세요"
-          value={password}
-          onChangeText={setPassword}
-          autoComplete="password"
-          importantForAutofill="yes"
-          onSubmitEditing={onSubmit}
-        />
-      </View>
+    <DismissKeyBoardView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.form}>
+          <TextField
+            ref={emailRef}
+            placeholder="아이디를 입력하세요"
+            label="아이디"
+            value={id}
+            onChangeText={setId}
+            importantForAutofill="yes"
+            textContentType="emailAddress"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current?.focus()}
+            blurOnSubmit={false}
+            keyboardType="email-address"
+          />
+          <TextField
+            label="비밀번호"
+            ref={passwordRef}
+            secureTextEntry
+            placeholder="비밀번호를 입력하세요"
+            value={password}
+            onChangeText={setPassword}
+            autoComplete="password"
+            importantForAutofill="yes"
+            onSubmitEditing={onSubmit}
+          />
+        </View>
 
-      <View style={styles.buttonContainer}>
-        <Pressable
-          style={[styles.button, styles.submitButton]}
-          onPress={onSubmit}
-          disabled={!isValid}>
-          <Text>로그인</Text>
-        </Pressable>
-        <Pressable
-          style={[styles.button, styles.submitButton]}
-          onPress={goToSignUp}>
-          <Text>회원가입하기</Text>
-        </Pressable>
+        <View style={styles.buttonContainer}>
+          <Pressable
+            style={[styles.button, styles.submitButton]}
+            onPress={onSubmit}
+            disabled={!isValid}>
+            <Text>로그인</Text>
+          </Pressable>
+          <Pressable
+            style={[styles.button, styles.submitButton]}
+            onPress={goToSignUp}>
+            <Text>회원가입하기</Text>
+          </Pressable>
+        </View>
       </View>
-    </View>
+    </DismissKeyBoardView>
   );
 };
 
 export default SignInPage;
 
 const styles = StyleSheet.create({
-  form: {
-    flex: 1,
+  container: {
+    height: Dimensions.get('window').height - 90,
     paddingHorizontal: 16,
     paddingVertical: 32,
   },
-  buttonContainer: {
-    alignItems: 'center',
+  form: {
+    flex: 1,
   },
+  buttonContainer: {},
   button: {
     width: '100%',
     paddingVertical: 16,
