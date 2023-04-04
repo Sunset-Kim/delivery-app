53e9f62f41291dc8cc699021d7772d25ccd31181
Sunset-Kim <0chickim@gmail.com>
2023-04-05 08:06:06 +0900

    feat: setup nmaps

--- android/app/src/main/java/com/sunset/fooddeliveryapp/fooddeliveryapp/MainApplication.java
+++ android/app/src/main/java/com/sunset/fooddeliveryapp/fooddeliveryapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.fooddeliveryapp;
+package com.sunset.fooddeliveryapp;
 
 import android.app.Application;
 import android.content.Context;
@@ -66,7 +66,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.fooddeliveryapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.sunset.fooddeliveryapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
